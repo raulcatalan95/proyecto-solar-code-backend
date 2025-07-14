305d2be79ddbb9654b46b65bd96d14cdf45f107f
Bastian <bastiancampusanode@gmail.com>
2025-07-13 21:59:29 -0400

    config: codeapplication

--- src/main/java/com/solar/code/CodeApplication.java
+++ src/main/java/com/solar/code/CodeApplication.java
@@ -2,21 +2,24 @@ package com.solar.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan; // 👈 importante
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.solar.code.model") // 👈 esto permite que detecte tus entidades
 public class CodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeApplication.class, args);
 	}
+
 	@Configuration
-	public static class Myconfiguration{
+	public static class Myconfiguration {
 		@Bean
-		public WebMvcConfigurer corsConfigurer(){
+		public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurer() {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
