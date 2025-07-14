bf642d5e70bc1c95ee690ad0bae488b5291c80c7
Bastian <bastiancampusanode@gmail.com>
2025-07-14 05:50:43 -0400

    codigoql

--- src/main/java/com/solar/code/CodeApplication.java
+++ src/main/java/com/solar/code/CodeApplication.java
@@ -2,14 +2,14 @@ package com.solar.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan; // 👈 importante
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-@EntityScan(basePackages = "com.solar.code.model") // 👈 esto permite que detecte tus entidades
+@SpringBootApplication(scanBasePackages = "com.solar.code")
+@EntityScan(basePackages = "com.solar.code.model")
 public class CodeApplication {
 
 	public static void main(String[] args) {
